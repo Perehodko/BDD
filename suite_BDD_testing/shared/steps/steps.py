8fa6ff4c4f770de3a28f45272ed6ebe60c38b1da
Perehodko <n.perehodko@yandex.com>
2020-01-20 13:40:57 +0300

    add tests

--- suite_BDD_testing/shared/steps/steps.py
+++ suite_BDD_testing/shared/steps/steps.py
@@ -50,3 +50,41 @@ def step(context):
     test.compare(waitForObjectExists(names.file_0_1_QModelIndex).text, "")
     test.compare(waitForObjectExists(names.file_0_2_QModelIndex).text, "")
     test.compare(waitForObjectExists(names.file_0_3_QModelIndex).text, "")
+
+
+@Then("table should have zero entries")
+def step(context):
+    test.compare(waitForObjectExists(names.cSV_Table_File_QTableWidget).rowCount, 0)
+
+
+@Given("I create a new table with two entries")
+def step(context):
+    clickButton(waitForObject(names.cSV_Table_New_QToolButton))
+    sendEvent("QMoveEvent", waitForObject(names.cSV_Table_Column_Names_QInputDialog), 502, 252, 284, 129)
+    type(waitForObject(names.enter_a_comma_separated_list_of_column_names_QLineEdit), "name, surname, email, phone")
+    type(waitForObject(names.enter_a_comma_separated_list_of_column_names_QLineEdit), "<Return>")
+    mouseClick(waitForObjectItem(names.cSV_Table_Unnamed_File_QTableWidget, "0/0"), 42, 9, Qt.NoModifier, Qt.LeftButton)
+    type(waitForObject(names.cSV_Table_Unnamed_File_QTableWidget), "I")
+    type(waitForObject(names.file_QExpandingLineEdit), "Anya")
+    type(waitForObject(names.file_QExpandingLineEdit), "<Tab>")
+    type(waitForObject(names.file_QExpandingLineEdit), "Kay")
+    type(waitForObject(names.file_QExpandingLineEdit), "<Tab>")
+    type(waitForObject(names.file_QExpandingLineEdit), "anyakay@g.com")
+    type(waitForObject(names.file_QExpandingLineEdit), "<Tab>")
+    type(waitForObject(names.file_QExpandingLineEdit), "1234")
+    mouseClick(waitForObjectItem(names.cSV_Table_Unnamed_File_QTableWidget, "0/0"), 48, 8, Qt.NoModifier, Qt.LeftButton)
+    openContextMenu(waitForObject(names.file_QHeaderView), 12, 38, Qt.NoModifier)
+    activateItem(waitForObjectItem(names.cSV_Table_Unnamed_File_QMenu, "Append Row"))
+    doubleClick(waitForObjectItem(names.cSV_Table_Unnamed_File_QTableWidget, "1/0"), 53, 17, Qt.NoModifier, Qt.LeftButton)
+    type(waitForObject(names.file_QExpandingLineEdit), "Lily")
+    doubleClick(waitForObjectItem(names.cSV_Table_Unnamed_File_QTableWidget, "1/1"), 3, 12, Qt.NoModifier, Qt.LeftButton)
+    type(waitForObject(names.file_QExpandingLineEdit), "Gen")
+    doubleClick(waitForObjectItem(names.cSV_Table_Unnamed_File_QTableWidget, "1/2"), 70, 10, Qt.NoModifier, Qt.LeftButton)
+    type(waitForObject(names.file_QExpandingLineEdit), "lilygen@y.com")
+    doubleClick(waitForObjectItem(names.cSV_Table_Unnamed_File_QTableWidget, "1/3"), 59, 17, Qt.NoModifier, Qt.LeftButton)
+    type(waitForObject(names.file_QExpandingLineEdit), "1333")
+    mouseClick(waitForObject(names.cSV_Table_Unnamed_File_QTableWidget), 701, 93, Qt.NoModifier, Qt.LeftButton)
+
+@Then("'2' entries should bepresent")
+def step(context):
+    test.compare(waitForObjectExists(names.cSV_Table_Unnamed_File_QTableWidget).rowCount, 2)
--- suite_BDD_testing/tst_case1/test.feature
+++ suite_BDD_testing/tst_case1/test.feature
@@ -1,8 +1,20 @@
 
 Feature: A new table
 
-    Scenario: The address book should be blank
+     Scenario: Initial state of created table
+         Given the application is running
+         Then table should have zero entries
 
+    Scenario: The address book should be blank
         Given the application is running
          When a new table is created
          Then the table should be blank
+
+	Scenario: State after adding two entries
+	    Given the application is running
+	    And I create a new table with two entries
+	    | name | surname | email 		| phone |
+	    | Anya | Kay     | anyakay@g.com| 1234  |
+	    | Lily | Gen     | lilygen@y.com| 1333  |
+	    Then '2' entries should bepresent
+
